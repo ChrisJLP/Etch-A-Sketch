cc98ddf2cb992c440f3afc18896f3290a373fa97
Chris Pring <Chrisjlpring@outlook.com>
2021-06-17 14:57:08 +0100

    Centered the container horizontally and vertically

--- EtchASketch.js
+++ EtchASketch.js
@@ -1,14 +1,13 @@
 const container = document.querySelector('#container');
 
+let gridSize = 16;
 
-
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < gridSize; i++) {
     const aDiv = document.createElement('div');
-    //aDiv.classList.add('gridRow');
+    aDiv.classList.add('gridRow');
     container.appendChild(aDiv);
-    for (j = 0; j < 16; j++) {
+    for (j = 0; j < gridSize; j++) {
         const anotherDiv = document.createElement('div');
-        //anotherDiv.textContent="test";
         anotherDiv.classList.add('gridDiv');
         aDiv.appendChild(anotherDiv);
     }
@@ -18,7 +17,14 @@ const squares = document.querySelectorAll('.gridDiv');
 console.log(squares);
 
 squares.forEach((square) => {
+    square.style.minHeight = `${(100 / gridSize)}%`; //change the min height of each square depending on amount of squares
     square.addEventListener('mouseover', () => {
         square.style.backgroundColor = 'black';
     });
 });
+
+const rows = document.querySelectorAll('.gridRow');
+
+rows.forEach((row) => {
+    row.style.minWidth = `${(100 / gridSize)}%`;
+})
